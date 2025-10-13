12214810eee0858ae62d162e9a3f827e9145be3e
Chanok Pathompatai <cpathompatai@gmail.com>
2025-10-13 16:40:21 +0700

    auto set latest ip when login

--- cms/server/contest/handlers/main.py
+++ cms/server/contest/handlers/main.py
@@ -250,16 +250,9 @@ class LoginHandler(ContestHandler):
         else:
             # auto set IP if login successfully (efited by Chanok)
             ip_address_str = str(ip_address)
-
-            # safe_ip_to_ipv4_str = lambda ip: str(
-            #     ipaddress.ip_network(ip, strict=False).network_address.ipv4_mapped
-            #     or ipaddress.ip_network(ip, strict=False).network_address
-            # )
-            # ip_address_str = safe_ip_to_ipv4_str(ip_address_str)
-
             if participation.ip == None:
                 participation.ip = [ip_address_str]
-            
+
             # multi ip address
             # if ip_address not in participation.ip:
             #     participation.ip.append(ip_address_str)
