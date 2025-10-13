139aedb5e537b29237f066413491bba46fd3bfff
Chanok Pathompatai <cpathompatai@gmail.com>
2025-10-13 16:22:21 +0700

    ipv6 to ipv4

--- cms/server/admin/handlers/__init__.py
+++ cms/server/admin/handlers/__init__.py
@@ -54,6 +54,7 @@ from .contestuser import \
     ContestUsersHandler, \
     RemoveParticipationHandler, \
     AddContestUserHandler, \
+    ClearParticipationIPHandler, \
     ParticipationHandler, \
     MessageHandler
 from .dataset import \
@@ -127,6 +128,7 @@ HANDLERS = [
 
     (r"/contest/([0-9]+)/users", ContestUsersHandler),
     (r"/contest/([0-9]+)/users/add", AddContestUserHandler),
+    (r"/contest/([0-9]+)/users/clearip", ClearParticipationIPHandler), # edit by Chanok
     (r"/contest/([0-9]+)/user/([0-9]+)/remove", RemoveParticipationHandler),
     (r"/contest/([0-9]+)/user/([0-9]+)/edit", ParticipationHandler),
     (r"/contest/([0-9]+)/user/([0-9]+)/message", MessageHandler),
